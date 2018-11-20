ef6201ab794ac96f62b43de72a7afda9e659d5be
24gaurangi <gwanjari19@gmail.com>
2018-11-20 15:20:04 -0800

    Add file

---
+++ src/AddTodo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// const Todos = ({todos, deleteTodo}) => {
+//   const todoList = todos.length ? (todos.map(todo => {
+//     return(
+//       <div className="collection-item" key={todo.id}>
+//         <span onClick = {() => {deleteTodo(todo.id)} } >{todo.content}</span>
+//       </div>
+//     )
+//   })) : (<p className=" center">You have no items left! </p>)
+//
+//   return(
+//     <div className="todos collection">
+//       {todoList}
+//     </div>
+//   )
+//
+// }
+
+export default AddTodo
--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     this.setState(
       {
         todos
-      }    )
+      })
   }
   render() {
     return (
